21dbfc605454f4e428101b6a0a913f03ceec177a
jalenallen1203-cloud <jalenallen1203@gmail.com>
2026-01-28 15:14:47 -0600

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ const CONFIG = {
     // They show up depending on how far they slide the meter
     loveMessages: {
         extreme: "WOOOOW You love me that much??",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
+        high: "To infinity and beyond!",              // Shows when they go past 1000%
         normal: "And beyond! 🥰"                           // Shows when they go past 10000%
     },
 
